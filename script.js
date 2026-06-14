28974fc7f449fe1cbf88c9d8ae89561dd14aa292
heruni <herunisp@gmail.com>
2026-06-14 14:38:59 +0530

    create get computer choice function

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        return "rock";
+     } else if (randomNumber === 1) {
+        return "paper";
+     } else {
+        return "scissor";   
+    }
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
